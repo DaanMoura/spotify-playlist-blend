dafa01d84c5da82f2c3e076c5aa4ce03305801c9
Daniel Moura <daniel.lucio.moura@gmail.com>
2023-12-03 22:16:38 -0300

    fix env file

--- app/env.ts
+++ app/env.ts
@@ -1,5 +1,6 @@
 export const env = {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID as string,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET as string,
-    JWT_SECRET: (process.env.NODE_ENV === 'production' ? process.env.NEXTAUTH_SECRET : process.env.NEXT_PUBLIC_JWT_SECRET) as string,
+    JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET as string,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string
 }
